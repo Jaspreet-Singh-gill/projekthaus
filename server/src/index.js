d2ef8a0524b68d2bc501eb5af5beabcadc5a760d
jaspreet singh <jaspreetsinghdall@gmail.com>
2026-01-09 23:19:56 +0530

    connected the mongoDB database

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,23 @@
 import dotenv from "dotenv";
-import app from "./app/app.js"
-
-
+import app from "./app/app.js";
+import { connectDB } from "./db/database.js";
 
 dotenv.config({
-    path:"./.env"
+  path: "./.env",
 });
-
 
 const PORT = process.env.PORT;
 
-
-app.listen(PORT||3000,()=>{
-    console.log(`The server is listning at the port ${PORT}`);
-}
-);
+connectDB()
+  .then(() => {
+    app.listen(PORT || 3000, () => {
+      console.log(`The server is listning at the port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(
+      "An error has occured while connecting to the database ",
+      error,
+    );
+    process.exit(1);
+  });
