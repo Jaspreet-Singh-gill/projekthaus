1cc3564df3580bd98d6a1dbc7a05099c4e6aa484
jaspreet singh <jaspreetsinghdall@gmail.com>
2026-01-10 14:40:53 +0530

    added hooks and methods to user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,8 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const user = Schema(
   {
@@ -72,5 +75,56 @@ const user = Schema(
     timestamps: true,
   },
 );
+
+//pre hooks before save to hash the password if it is modiefied
+user.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  user.password = await bcrypt.hash(this.password, 10);
+});
+
+//a method on user model to check the password
+user.modules.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+user.modules.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECREAT,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+user.modules.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECREAT,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+user.modules.generateTempararyTokens = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 20 * 60 * 1000;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
 
 export const User = mongoose.Model("User", user);
