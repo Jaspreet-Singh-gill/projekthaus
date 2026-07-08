7ff88e8ba2d008cc0839ad7bf82e12704775441d
jaspreet singh <84448984+Jaspreet-Singh-gill@users.noreply.github.com>
2026-07-08 17:53:15 +0530

    Enable secure transport for mail configuration

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -5,6 +5,8 @@ import { ApiError } from "./apiErrorResponse.js";
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
   port: process.env.MAILTRAP_PORT,
+  secure: true,
+  requireTLS: true,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
