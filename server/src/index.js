6d00068e3d9f9b53ad4362c61030aec84952df9e
jaspreet singh <jaspreetsinghdall@gmail.com>
2026-07-23 22:30:34 +0530

    installed socket.io and wraped the express app into the http server

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import app from "./app/app.js";
 import { connectDB } from "./db/database.js";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config({
   path: "./.env",
@@ -8,9 +10,31 @@ dotenv.config({
 
 const PORT = process.env.PORT;
 
+const httpServer = http.createServer(app);
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CORS_ORIGIN?.split(","),
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on("join_room", (userId) => {
+    socket.join(userId);
+    console.log(`User ${userId} joined room ${userId}`);
+  });
+  
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected: ${socket.id}`);
+  });
+});
+
 connectDB()
   .then(() => {
-    app.listen(PORT || 3000, () => {
+    httpServer.listen(PORT || 3000, () => {
       console.log(`The server is listning at the port ${PORT}`);
     });
   })
